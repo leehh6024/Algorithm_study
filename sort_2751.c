4ddfbe15d78b2ed87ea36d1a83236229a45f9869
leehh6024 <dlgusghchlrh@gmail.com>
2022-03-06 20:37:59 +0900

    상금은 영어로 prize

--- sort_2751.c
+++ sort_2751.c
@@ -1,30 +1,31 @@
-#include <stdio.h>
-
-int main()
-{
-	int n=0;
-	int num[10] = { 0};
-	int tmp = 0;
-
-	scanf_s("%d", &n);
-
-	for (int i = 0; i < n; i++) {
-		scanf_s("%d", &num[i]);
-	}
-
-	for (int i = 0; i < n - 1; i++) {
-		for (int j = 0; j < n - 1; j++) {
-			if (num[j] > num[j + 1]) {
-				tmp = num[j];
-				num[j] = num[j + 1];
-				num[j + 1] = tmp;
-			}
-		}
-	}
-
-	for (int i = 0; i < n; i++) {
-		printf("%d\n", num[i]);
-	}
-
-	return 0;
-}
+//#include <stdio.h>
+//
+//int num[1000000] = { 0 };
+//
+//int main()
+//{
+//	int n=0;
+//	int tmp = 0;
+//
+//	scanf_s("%d", &n);
+//
+//	for (int i = 0; i < n; i++) {
+//		scanf_s("%d", &num[i]);
+//	}
+//
+//	for (int i = 0; i < n - 1; i++) {
+//		for (int j = 0; j < n - 1; j++) {
+//			if (num[j] > num[j + 1]) {
+//				tmp = num[j];
+//				num[j] = num[j + 1];
+//				num[j + 1] = tmp;
+//			}
+//		}
+//	}
+//
+//	for (int i = 0; i < n; i++) {
+//		printf("%d\n", num[i]);
+//	}
+//
+//	return 0;
+//}
